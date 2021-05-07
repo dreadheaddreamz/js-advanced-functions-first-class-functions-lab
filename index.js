2f4bfd0c8a3451267252615a9f9869b1d44337b2
dreadheaddreamz <github email address>
2021-05-07 02:54:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-// Code your solution in this file!
-const firstTwoDrivers = function (drivers) {
+const returnFirstTwoDrivers = function (drivers) {
     return drivers.slice(0, 2);
-};
-
-const returnLastTwoDrivers = function (drivers) {
+  };
+  
+  const returnLastTwoDrivers = function (drivers) {
     return drivers.slice(-2);
   };
   
